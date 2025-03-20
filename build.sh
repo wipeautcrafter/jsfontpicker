eba6ed54c7c0f705ccb6eb02b3c28f09cc183fc2
Wiebe Beeftink <wipeautcrafter@gmail.com>
2025-03-20 10:43:50 +0100

    chore: update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+MANAGER="npm"
+RUNNER="npx"
+
 RESET="\033[0m"
 PURPLE="\033[1;95m"
 GREEN="\033[1;92m"
@@ -14,17 +17,24 @@ log_error() {
   exit $?
 }
 
+# Find Node package manager
+if [ -x "$(command -v bun)" ]; then
+  MANAGER="bun"
+  RUNNER="bunx"
+fi
+echo -e "$GREEN(using $MANAGER)$RESET"
+
 # Clear build directory
 rm -f dist/*
 
 # Generate types
 log_step "🔗 Generating types... "
-ERROR=$(npm run tsc -- --pretty) || log_error
+ERROR=$($MANAGER run tsc 2>&1 >/dev/null) || log_error
 log_done
 
 # Build source
 log_step "🔨 Building source...  "
-ERROR=$(npm run build 2>&1 >/dev/null) || log_error
+ERROR=$($MANAGER run build 2>&1 >/dev/null) || log_error
 log_done
 
 # Minify files
@@ -34,7 +44,7 @@ shopt -s nullglob
 for file in *.{js,css}; do
   name=${file%.*}
   ext=${file##*.}
-  ERROR=$(esbuild $file --minify --outfile="$name.min.$ext" 2>&1 >/dev/null) || log_error
+  ERROR=$($RUNNER esbuild $file --minify --outfile="$name.min.$ext" 2>&1 >/dev/null) || log_error
 done
 log_done
 shopt -u nullglob
